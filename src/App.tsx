fe1ad3c40ce1fca534a6f7d17458c22091ea9deb
Rayane300402 <raycaratboulsi@gmail.com>
2026-01-12 18:25:59 +0200

    set up product view section - not completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import ProductView from "./components/ProductView"
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductView />
     </main>
   )
 }
